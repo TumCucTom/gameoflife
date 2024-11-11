6835a6dba74fb597701bd57df84cf061aa41552d
Thomas Bale <tokbale@outlook.com>
2024-11-11 06:29:59 +0000

    ready for zip

--- bench_test.go
+++ bench_test.go
@@ -13,15 +13,15 @@ import (
 const benchLength = 1000
 
 func BenchmarkStudentVersion(b *testing.B) {
-	for threads := 1; threads <= 16; threads++ {
+	for threads := 1; threads <= 3; threads++ {
 		os.Stdout = nil // Disable all program output apart from benchmark results
 		p := gol.Params{
 			Turns:       benchLength,
-			Threads:     threads,
+			Threads:     10,
 			ImageWidth:  512,
 			ImageHeight: 512,
 		}
-		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, 1)
 		b.Run(name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				events := make(chan gol.Event)
--- gol/distributor.go
+++ gol/distributor.go
@@ -8,11 +8,8 @@ import (
 )
 
 var wg, pause, executingKeyPress, pauseReached sync.WaitGroup
-
-// var mu sync.Mutex
 var turn IntContainer
-
-var worldGlobal, worldOld [][]uint8
+var worldGlobal [][]uint8
 
 type BoolContainer struct {
 	mu      sync.Mutex
@@ -73,9 +70,6 @@ func (c *BoolContainer) get() bool {
 
 var end, snapshot, getCount BoolContainer
 
-const CellAlive = 255
-const CellDead = 0
-
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -92,25 +86,15 @@ type pixel struct {
 	Value uint8
 }
 
-type suint8 struct {
-	x   int
-	val []uint8
-}
-
-type neighbourPixel struct {
-	X    int
-	Y    int
-	quit bool
-}
-
 func getAliveCells(p Params, world [][]uint8) []util.Cell {
 	// make the slice
 	aliveCells := make([]util.Cell, 0)
 
+	// check all cells and if alive append it
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
-			// append every CellAlive cells
-			if world[x][y] == CellAlive {
+			// append every 255 cells
+			if world[x][y] == 255 {
 				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
 			}
 		}
@@ -121,10 +105,12 @@ func getAliveCells(p Params, world [][]uint8) []util.Cell {
 
 func getNumAliveCells(p Params, world [][]uint8) int {
 	num := 0
+
+	// iterate through whole board
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
-			// append every CellAlive cells
-			if world[x][y] == CellAlive {
+			// add 1 for every alive cell
+			if world[x][y] == 255 {
 				num++
 			}
 		}
@@ -137,8 +123,10 @@ func makeOutput(p Params, c distributorChannels, world [][]uint8) {
 
 	// add a get output to the command channel
 	c.ioCommand <- ioOutput
+	// give filename
 	c.ioFilename <- fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, p.Turns)
 
+	// pass the world slice
 	c.ioOutput <- world
 
 	// Make sure that the Io has finished any output before exiting.
@@ -150,9 +138,11 @@ func makeOutputTurnWithTurnNum(p Params, c distributorChannels, turns int, world
 
 	// add a get output to the command channel
 	c.ioCommand <- ioOutput
+	// give filename with specified turns
 	filename := fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, turns)
 	c.ioFilename <- filename
 
+	// pass the world slice
 	c.ioOutput <- world
 
 	// Make sure that the Io has finished any output before exiting.
@@ -162,19 +152,22 @@ func makeOutputTurnWithTurnNum(p Params, c distributorChannels, turns int, world
 }
 
 func calculateNewAliveParallel(p Params, workerNum int, c distributorChannels, world [][]uint8) [][]uint8 {
-	//numRows := p.ImageHeight / workerNum
-
+	// create channels for the workers to pass back their data on
 	splitSegments := make([]chan pixel, workerNum)
 	for i := range splitSegments {
 		splitSegments[i] = make(chan pixel, p.ImageWidth*p.ImageWidth)
 	}
+
 	// start workers to make the world
-	//startWorkers(workerNum, numRows, p, dataChan, neighbourChan, c)
 	setupWorkers(p.ImageHeight, workerNum, splitSegments, world)
 
+	// setup cells for SDL flipping
 	var cells []util.Cell
 
+	// wait for workers to finish processing
 	wg.Wait()
+
+	// for every worker, use their changed cell data to flip cells and update board
 	for i := 0; i < workerNum; i++ {
 		length := len(splitSegments[i])
 		for j := 0; j < length; j++ {
@@ -183,20 +176,18 @@ func calculateNewAliveParallel(p Params, workerNum int, c distributorChannels, w
 			cells = append(cells, util.Cell{X: item.X, Y: item.Y})
 		}
 	}
-	c.events <- CellsFlipped{turn.get() + 1, cells}
-	//for i := 0; i < workerNum; i++ {
-	//	//temp := <-splitSegments[i]
-	//	//for _, item := range temp {
-	//	//	fmt.Println(item)
-	//	//}
-	//	newWorld = append(newWorld, <-splitSegments[i]...)
-	//}
 
+	// flip cells for SDL
+	c.events <- CellsFlipped{turn.get() + 1, cells}
+
+	// return the new state of the board
 	return world
 }
 func setupWorkers(size, workerNum int, splitSegments []chan pixel, world [][]uint8) {
+	// the number of rows that the first n-1 workers should calculate
 	numRows := size / workerNum
 
+	// run the first n-1 workers for their given rows
 	i := 0
 	for i < workerNum-1 {
 		wg.Add(1)
@@ -209,27 +200,27 @@ func setupWorkers(size, workerNum int, splitSegments []chan pixel, world [][]uin
 	go runWorker(size, i*numRows, size, splitSegments[i], world)
 }
 func runWorker(size, start, end int, splitSegment chan pixel, world [][]uint8) {
+	// state you are done when returned from this function
 	defer wg.Done()
+
+	// add relevant changes to worker specific channel
 	calculateNextWorld(start, end, size, world, splitSegment)
 }
 
 func calculateNextWorld(start, end, width int, world [][]uint8, c chan pixel) {
-	//newWorld := make([][]uint8, end-start)
-	//for i := 0; i < end-start; i++ {
-	//	newWorld[i] = world[i]
-	//}
-
+	// calculate relevant neighbours
 	neighboursWorld := calculateNeighbours(start, end, width, world)
-	//for _, item := range neighboursWorld {
-	//	fmt.Println(item)
-	//}
 
+	// for all cells in this workers range
+	// send an update if one is needed according to GoL rules
 	for y := start; y < end; y++ {
 		for x := 0; x < width; x++ {
 			neighbors := neighboursWorld[y][x]
 			if (neighbors < 2 || neighbors > 3) && world[y][x] == 255 {
+				// if an alive cell becomes dead
 				c <- pixel{y, x, 0}
 			} else if neighbors == 3 && world[y][x] == 0 {
+				// if a dead cell becomes alive
 				c <- pixel{y, x, 255}
 			}
 		}
@@ -237,15 +228,19 @@ func calculateNextWorld(start, end, width int, world [][]uint8, c chan pixel) {
 }
 
 func calculateNeighbours(start, end, width int, world [][]uint8) [][]int {
+	//setup slice to hold neighbours
 	neighbours := make([][]int, width)
 	for i := range neighbours {
 		neighbours[i] = make([]int, width)
 	}
 
+	// if you are not calculating the whole board yourself ( 1 worker)
 	if !(start == 0 && end == width) {
+		// if you are the first worker
 		if start == 0 {
+			// get wrap around data from the final row
 			for x := 0; x < width; x++ {
-				if world[width-1][x] == CellAlive {
+				if world[width-1][x] == 255 {
 					for i := -1; i <= 1; i++ {
 						//for image wrap around
 						xCoord := x + i
@@ -259,12 +254,15 @@ func calculateNeighbours(start, end, width int, world [][]uint8) [][]int {
 				}
 			}
 		} else {
+			// otherwise start calculating from one row above your own
 			start--
 		}
 
+		// if you are the final worker
 		if end == width {
+			// get neighbours from the first row wrap around
 			for x := 0; x < width; x++ {
-				if world[0][x] == CellAlive {
+				if world[0][x] == 255 {
 					for i := -1; i <= 1; i++ {
 						//for image wrap around
 						xCoord := x + i
@@ -278,14 +276,16 @@ func calculateNeighbours(start, end, width int, world [][]uint8) [][]int {
 				}
 			}
 		} else {
+			// otherwise calculate neighbours for one row below
 			end++
 		}
 	}
 
+	// for all cells in your calculating range
 	for y := start; y < end; y++ {
 		for x := 0; x < width; x++ {
-			// if a cell is CellAlive
-			if world[y][x] == CellAlive {
+			// if a cell is 255
+			if world[y][x] == 255 {
 				// add 1 to all neighbours
 				// i and j are the offset
 				for i := -1; i <= 1; i++ {
@@ -293,6 +293,8 @@ func calculateNeighbours(start, end, width int, world [][]uint8) [][]int {
 
 						// if you are not offset, do not add one. This is yourself
 						if !(i == 0 && j == 0) {
+
+							// wraparound
 							ny, nx := y+i, x+j
 							if nx < 0 {
 								nx = width - 1
@@ -310,6 +312,7 @@ func calculateNeighbours(start, end, width int, world [][]uint8) [][]int {
 								ny = ny % width
 							}
 
+							// add neighbour count
 							neighbours[ny][nx]++
 						}
 
@@ -330,6 +333,7 @@ func runAliveEvery2(done chan bool) {
 		//see if we need to report the number of alive cells
 		select {
 		case <-done:
+			// stop checking
 			return
 		case _ = <-ticker.C:
 			getCount.setTrue()
@@ -338,35 +342,48 @@ func runAliveEvery2(done chan bool) {
 }
 
 func paused(c distributorChannels, p Params, world [][]uint8, turns int) {
+	//state that the game is paused and continually check for key presses
 	c.events <- StateChange{turn.get(), Paused}
 	for keyNew := range c.keyPresses {
 		switch keyNew {
 		case 's':
+			// make PGM output for the current turn and world state
 			makeOutputTurnWithTurnNum(p, c, turns, world)
 		case 'p':
+			// unpause and state that execution has started again
 			c.events <- StateChange{turn.get(), Executing}
 			pause.Done()
+			// go back to original key press function
 			return
 		case 'q':
+			// unpause and quit the game
 			end.setTrue()
 			pause.Done()
+			// go back to original key press function
 			return
 		}
 	}
 }
 
 func runKeyPressController(c distributorChannels, p Params) {
+	// continually check for key presses
 	for key := range c.keyPresses {
 		switch key {
 		case 's':
+			// take a snapshot after this turn finishes calculating
 			snapshot.setTrue()
 		case 'q':
+			// quit after this turn finishes calculating
 			end.setTrue()
 			return
 		case 'p':
+			// stop the game from quitting as you are performing a key press
 			executingKeyPress.Add(1)
+			// pause processing
 			pause.Add(1)
+			// wait for the pause to go into action
 			pauseReached.Wait()
+			// check for further key presses whilst paused
 			paused(c, p, worldGlobal, turn.get())
 			executingKeyPress.Done()
 		}
@@ -374,35 +391,42 @@ func runKeyPressController(c distributorChannels, p Params) {
 }
 
 func executeTurns(p Params, c distributorChannels, world [][]uint8) [][]uint8 {
+	// start this wait group for pausing utiity
 	pauseReached.Add(1)
+
+	//execute all turn for the GoL
 	for turn.get() < p.Turns {
-		// call the function to calculate new CellAlive cells from old CellAlive cells
+		// call the function to calculate new 255 cells from old 255 cells
 		world = calculateNewAliveParallel(p, p.Threads, c, world)
 		worldGlobal = world
 
 		// increase the number of turns passed
 		turn.inc()
-		//runKeyPressController(c, p)
 
+		//pause utility
 		pauseReached.Done()
 		pause.Wait()
 		pauseReached.Add(1)
 
+		// if the game ends prematurely, exit this function with current world
 		if end.get() {
 			return world
 		}
+
+		// if we should return the count, get it a return it
 		if getCount.get() {
 			getCount.setFalse()
 			c.events <- AliveCellsCount{turn.get(), getNumAliveCells(p, world)}
 		}
+
+		// if we should output a pgm, do so
 		if snapshot.get() {
 			snapshot.setFalse()
 			makeOutputTurnWithTurnNum(p, c, turn.get(), world)
 		}
 
+		// send turn complete for SDL
 		c.events <- TurnComplete{turn.get()}
-		worldOld = world
-
 	}
 	return world
 }
@@ -415,6 +439,7 @@ func distributor(p Params, c distributorChannels) {
 		world[i] = make([]uint8, p.ImageWidth)
 	}
 
+	// set turns to 0
 	turn.reset()
 
 	// add a get input to the command channel
@@ -422,11 +447,17 @@ func distributor(p Params, c distributorChannels) {
 	c.ioCommand <- ioInput
 	c.ioFilename <- fmt.Sprintf("%vx%v", p.ImageWidth, p.ImageHeight)
 
+	// for SDL flips
 	cells := make([]util.Cell, p.ImageWidth*p.ImageHeight)
 
+	//get the world
 	world = <-c.ioInput
-	worldOld = world
 
+	// Make sure that the Io has finished any input before moving on to processing
+	c.ioCommand <- ioCheckIdle
+	<-c.ioIdle
+
+	// get flipped cells
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			if world[x][y] == 255 {
@@ -435,11 +466,10 @@ func distributor(p Params, c distributorChannels) {
 		}
 	}
 
+	// setup global
 	worldGlobal = world
-	//worldGlobal.setup(world)
-	//for _, n := range worldGlobal.giveWhole() {
-	//	fmt.Println(n)
-	//}
+
+	// flip the cells are start execution
 	c.events <- CellsFlipped{turn.get(), cells}
 	c.events <- StateChange{turn.get(), Executing}
 
@@ -451,10 +481,6 @@ func distributor(p Params, c distributorChannels) {
 	// run the ticker
 	done := make(chan bool)
 	go runAliveEvery2(done)
-
-	// Make sure that the Io has finished any input before moving on to processing
-	c.ioCommand <- ioCheckIdle
-	<-c.ioIdle
 
 	// Execute all turns of the Game of Life.
 	world = executeTurns(p, c, world)
